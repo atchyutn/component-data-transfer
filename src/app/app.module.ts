7687ab5b30be7acc4b6e8bd280c03ccaf7b36f7d
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2022-02-26 10:04:12 +0530

    Created three components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponentComponent } from './child-component/child-component.component';
+import { ParentComponentComponent } from './parent-component/parent-component.component';
+import { SiblingComponentComponent } from './sibling-component/sibling-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponentComponent,
+    ParentComponentComponent,
+    SiblingComponentComponent
   ],
   imports: [
     BrowserModule,
